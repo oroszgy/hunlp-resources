929c3f4acf27487994912b1a2b507b28b9a251eb
Gyorgy Orosz <oroszgy@gmail.com>
2016-12-28 23:14:31 +0100

    Initial wikipedia processing scripts

---
+++ corpora/extract_pages.sh
@@ -0,0 +1,1 @@
+../modules/wikiextractor/WikiExtractor.py -ns 0 --filter_disambig_pages -o ./wiki_docs -b 100M ./huwiki-20161220-pages-articles.xml.bz2
---
+++ corpora/get_wikidump.sh
@@ -0,0 +1,1 @@
+wget https://dumps.wikimedia.org/huwiki/20161220/huwiki-20161220-pages-articles.xml.bz2
