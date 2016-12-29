c50773be14c6cac5dc9bfc44797c91ee913957b1
Gyorgy Orosz <oroszgy@gmail.com>
2016-12-29 23:04:13 +0100

    Updated scripts for preprocessing the corpus.

---
+++ corpora/brown_clustering.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+../../brown-cluster/wcluster --text corpus_sents_clean.txt --c 1000 --threads 8
--- corpora/clean_corpus.py
+++ corpora/clean_corpus.py
@@ -2,11 +2,10 @@ import sys
 
 
 def is_clean(chars):
-    return sum(ch.islower() for ch in chars) / len(chars) > 0.9
+    return sum(ch.islower() for ch in chars) / sum(not ch.isspace() for ch in chars) > 0.9
 
 
 for line in sys.stdin:
     line = line.strip()
     if line and is_clean(line):
         print(line)
-
