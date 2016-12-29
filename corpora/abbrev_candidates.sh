02ec526e0bb981dd042f136b3ec331761542d138
Gyorgy Orosz <oroszgy@gmail.com>
2016-12-29 22:48:16 +0100

    Fixed abbrev finding script.

--- corpora/abbrev_candidates.sh
+++ corpora/abbrev_candidates.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-pv ./wiki_docs/*/wiki_* | tr " " "\n" | grep "\.$" | sort | uniq -c | sort -nr > abbrev_candidates.txt
+pv ./wiki_docs/*/wiki_* | tr " " "\n" | grep "[a-züóőúéáűí]\.$" | sort | uniq -c | sort -nr > abbrev_candidates.txt
