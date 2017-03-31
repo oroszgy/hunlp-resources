6d56b3efa09d6b8de7d1fb79a53eba2e9480ee66
Gyorgy Orosz <oroszgy@gmail.com>
2017-03-31 16:33:19 +0200

    bugfix

--- wordvectors/benchmark.sh
+++ wordvectors/benchmark.sh
@@ -1,7 +1,7 @@
 for dim in 100 150 300; do
     for min in  0 5 10; do 
         for cb in 0 1; do
-            python -m gensim.scripts.word2vec_standalone -train $1 -output w2v_d${dim}_m${min}_c${min} -size ${dim} -threads 4 -min_count ${min} -cbow ${cb} -accuracy ./questions-words-hu.txt &> w2v_d${dim}_m${min}_c${min}.log
+            python -m gensim.scripts.word2vec_standalone -train $1 -output w2v_d${dim}_m${min}_c${cb} -size ${dim} -threads 4 -min_count ${min} -cbow ${cb} -accuracy ./questions-words-hu.txt &> w2v_d${dim}_m${min}_c${cb}.log
         done
     done
 done
