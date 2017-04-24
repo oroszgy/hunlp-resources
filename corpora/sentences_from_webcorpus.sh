55765d39767d7deb66916722ab6d179ed2c74e4f
Gyorgy Orosz <oroszgy@gmail.com>
2017-04-24 09:03:39 +0000

    Improved tokenization scripts

--- corpora/sentences_from_webcorpus.sh
+++ corpora/sentences_from_webcorpus.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-cat $1 | iconv -f latin2 -t utf8 | awk '/^<s>/{print substr($0,4)}' > $2
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cat $1 | iconv -f latin2 -t utf8 | awk '/^<s>/{print substr($0,4)}' | python $DIR/tokenize_doc.py > $2
