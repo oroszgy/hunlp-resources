29d55702b73ee0d8b624cf05dad5e235ecc088b1
Gyorgy Orosz <oroszgy@gmail.com>
2017-05-24 22:23:13 +0200

    Improved webcorpus preprocessing

--- corpora/tokenize_webcorpus.py
+++ corpora/tokenize_webcorpus.py
@@ -13,27 +13,29 @@ TAG = re.compile(u"<[^<>]+>")
 
 def clean_line(line, min_char_ratio=0.9, min_length=50):
     line = ftfy.fix_text(line)
-    line = TAG.sub("", line.strip())
+    line = TAG.sub("", line)
     if line and len(line) > min_length:
         char_ratio = float(sum(ch.islower() for ch in line)) / len(line)
         if char_ratio > min_char_ratio:
-            yield line
+            return line
+        else:
+            return None
 
 
 def sentences(f):
     for line in f:
         if SENT_START.match(line):
-            line = ftfy.fix_text(line[3:].strip())
-            line = TAG.sub("", line)
-            yield " ".join(token.text for token in TOKENIZER(line))
+            line = clean_line(line[3:].strip())
+            if line:
+                yield " ".join(token.text for token in TOKENIZER(line))
 
 
 if __name__ == "__main__":
     outdir = sys.argv[2]
     for fpath in glob.iglob(sys.argv[1]):
         head, tail = os.path.split(fpath)
-        with open(fpath, encoding='iso-8859-2') as inpf, open("{}/{}".format(outdir, tail), "w",
-                                                              encoding="utf8") as outf:
+        with open(fpath, encoding='iso-8859-2') as inpf, \
+                open("{}/{}".format(outdir, tail), "w", encoding="utf8") as outf:
             for s in sentences(inpf):
                 outf.write(s)
                 outf.write("\n")
