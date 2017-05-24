514302b8b459650fdadf753bd8e98e38114b1055
Gyorgy Orosz <oroszgy@gmail.com>
2017-05-24 22:20:09 +0200

    Improved webcorpus preprocessing

--- corpora/tokenize_webcorpus.py
+++ corpora/tokenize_webcorpus.py
@@ -8,14 +8,23 @@ import spacy.hu
 
 TOKENIZER = spacy.hu.Hungarian().tokenizer
 SENT_START = re.compile("^<s>")
-TAGS = re.compile("<[^>]+>")
+TAG = re.compile(u"<[^<>]+>")
+
+
+def clean_line(line, min_char_ratio=0.9, min_length=50):
+    line = ftfy.fix_text(line)
+    line = TAG.sub("", line.strip())
+    if line and len(line) > min_length:
+        char_ratio = float(sum(ch.islower() for ch in line)) / len(line)
+        if char_ratio > min_char_ratio:
+            yield line
 
 
 def sentences(f):
     for line in f:
         if SENT_START.match(line):
             line = ftfy.fix_text(line[3:].strip())
-            line = TAGS.sub("", line)
+            line = TAG.sub("", line)
             yield " ".join(token.text for token in TOKENIZER(line))
 
 
